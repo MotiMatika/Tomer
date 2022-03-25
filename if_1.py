86dde39d5d8a006a600920526286faa4abffe4ec
MotiMatika <98161728+MotiMatika@users.noreply.github.com>
2022-03-25 17:03:56 +0300

    Update if_1.py

--- if_1.py
+++ if_1.py
@@ -7,3 +7,13 @@ if guess>x:
     print("תחצינ\n")
 else:
     print("תדספיה\n")
+
+# print(a,b)
+# c=int(input("\nwhich number is the biggest ? "))
+# if c==a or c==b: 
+#     if c>a or c>b:
+#         print("\nןוכנ\n")
+#     else:
+#         print("\nןוכנ אל \n")
+# else:
+#     print("\nעיפומ אל\n") 
