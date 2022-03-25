b44ed2cd2c4fac253cdf90248570cdc2dae214dd
MotiMatika <98161728+MotiMatika@users.noreply.github.com>
2022-03-25 17:25:40 +0300

    n1

--- if_1.py
+++ if_1.py
@@ -1,19 +1,29 @@
 import random
 x=random.randint(1,100)
 
-guess =int(input("\nךלש שוחינה : "))
-print("\nבשחמה לש שוחינה ",x,"\n")
-if guess>x:
-    print("תחצינ\n")
-else:
-    print("תדספיה\n")
-
-# print(a,b)
-# c=int(input("\nwhich number is the biggest ? "))
-# if c==a or c==b: 
-#     if c>a or c>b:
-#         print("\nןוכנ\n")
-#     else:
-#         print("\nןוכנ אל \n")
+# guess =int(input("\nךלש שוחינה : "))
+# print("\nבשחמה לש שוחינה ",x,"\n")
+# if guess>x:
+#     print("תחצינ\n")
 # else:
-#     print("\nעיפומ אל\n") 
+#     print("תדספיה\n")
+
+
+
+
+#לתומר
+import random as rd
+a=rd.randint(1,100)
+b=rd.randint(1,100)
+print("היי קוראים לי שחר")
+print(a,b)
+c=int(input("\nwhich number is the biggest ? "))
+if c==a or c==b: 
+    if c>a or c>b:
+        print("\nןוכנ\n")
+    else:
+        print("\nןוכנ אל \n")
+else:
+    print("\nעיפומ אל\n")
+
+ 
