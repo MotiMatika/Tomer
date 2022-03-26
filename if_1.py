db90cc0bf98d977a323e66712105ce83eb509652
MotiMatika <motimatika@gmail.com>
2022-03-26 18:11:36 +0300

    Update if_1.py

--- if_1.py
+++ if_1.py
@@ -12,13 +12,29 @@ x=random.randint(1,100)
 
 
 #מהו המספר הגדול ביותר מבין שניים
+# import random
+# a=random.randint(1,100)
+# b=random.randint(1,100)
+# print(a,b)
+# qu=int(input("\n? רתויב לודגה רפסמה "))
+# if qu==a or qu==b: 
+#     if qu>a or qu>b:
+#         print("\nדאמ הפי\n")
+#     else:
+#         print("\nןוכנ אל \n")
+# else:
+#     print("\nבוט האור אל התא\n")
+
+
+#מהו המספר הגדול ביותר מבין שניים
 import random
 a=random.randint(1,100)
 b=random.randint(1,100)
-print(a,b)
-c=int(input("\n? רתויב לודגה רפסמה "))
-if c==a or c==b: 
-    if c>a or c>b:
+c=random.randint(1,100)
+print(a,b,c)
+qu=int(input("\n? רתויב לודגה רפסמה "))
+if qu==a or qu==b qu==c: 
+    if qu>a or qu>b :
         print("\nדאמ הפי\n")
     else:
         print("\nןוכנ אל \n")
