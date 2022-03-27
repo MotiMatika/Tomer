733c24907d9715563357e5b15c34313b1a797317
MotiMatika <motimatika@gmail.com>
2022-03-27 21:37:15 +0300

    5

---
+++ Lesson_3_mivne_asori/3_digits.py
@@ -0,0 +1,13 @@
+briks=input("\nenter 3 digits - each for one pig "  )      #הכנסת מספר תלת ספרתי
+pig1=int(int(briks)/100)                                 #הפרדת ספרת המאות שמציינת את מספר הלבנים שאסף חזיר ראשון
+pig2=int((int(briks)/10-pig1*10))                        #הפרדת ספרת העשרות שמציינת את מספר הלבנים שאסף חזיר שני
+pig3=int(briks)-(pig1*100+pig2*10)                       #הפרדת ספרת האחדות שמציינת את מספר הלבנים שאסף חזיר שלישי
+sum=pig1+pig2+pig3                                       #חישוב סכום הלבנים שאספו שלשת החזירים
+print("\nsum of briks collected :",sum)                    #הדפסת הסכום
+get=int(sum/3)
+print("\neach pig gets :", get," briks")                   #חלוקה שווה של הלבנים - ללא שארית
+spare=sum-get*3
+print("\nspare of brick after dividing :",spare)           # פלט לשארית שנותרה לאחר החלוקה
+x=True
+x=spare==0                                               #בודק האם יש שארית.שאלתי האם השארית שווה לאפס
+print(x)  
