1322e7b684399b9cc09ca92f80e511adf7d3a320
MotiMatika <98161728+MotiMatika@users.noreply.github.com>
2022-03-28 10:34:06 +0300

    5

--- Lesson_2_if/if_2.py
+++ Lesson_2_if/if_2.py
@@ -2,4 +2,37 @@ temp = int(input("\n?הרוטרפמטה יהמ "))
 if temp <=15:
     print(" יל רק ...ררב")
 else:
-    print("יל םח")    
+    print("יל םח")   
+
+#להעביר למקבץ איף ולהבין מה ההבדל בין 2 הגירסאות
+# x=1
+# if x>4:
+#     print("x>4")
+# elif x==3:
+#     print("x=3")
+# else:
+#     print("x<3")
+
+
+# num = int(input("enter a number : "))
+# if num>4:
+#     print("number > 4")
+# elif num==3:
+#     print("number = 3")
+# else:
+#     print("number < 3")
+
+
+# letter = input("enter a letter : ")
+# if letter=="t":
+#     print("you are tomer")
+# elif letter=="u":
+#     print("you are uri")
+# elif letter=="h":
+#     print("you are hilla")
+# elif letter=="m":
+#     print("you are moti")
+# elif letter=="d":
+#     print("you are dudi")
+# else:
+#     print("you are no one") 
