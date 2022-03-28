a694484a092d684021760f9efeb7fe4d1688d44c
MotiMatika <motimatika@gmail.com>
2022-03-28 17:30:05 +0300

    5

--- Lesson_3_mivne_asori/2.digits.py
+++ Lesson_3_mivne_asori/2.digits.py
@@ -10,15 +10,10 @@
 
 #      @@@   פעילות 2  @@@
 # פיצול מספר תלת ספרתי לספרותיו
-
-
-
-
-
-num = int(input("\nףלאל רשע ןיב רפסמ סנכה "))
-meot = int(num/100)
-print("תואמה תרפס  ",meot)
-asarot = int((num-meot*100)/10)
-print("תורשעה תרפס ",asarot)
-ahadot = int(num-100*meot-10*asarot)
-print("תודחאה תרפס ",ahadot)
+# num = int(input("\nףלאל רשע ןיב רפסמ סנכה "))
+# meot = int(num/100)
+# print("תואמה תרפס  ",meot)
+# asarot = int((num-meot*100)/10)
+# print("תורשעה תרפס ",asarot)
+# ahadot = int(num-100*meot-10*asarot)
+# print("תודחאה תרפס ",ahadot)
