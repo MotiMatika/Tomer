ec366185b59f9a5bd4c4ccedb00246b0ef278789
MotiMatika <98161728+MotiMatika@users.noreply.github.com>
2022-03-28 10:53:21 +0300

    5

--- Lesson_2_if/if_2.py
+++ Lesson_2_if/if_2.py
@@ -6,13 +6,13 @@
 
 #להבין מה ההבדל בין 2 הגירסאות
 #גירסא 1
-# x=1
-# if x>4:
-#     print("x>4")
-# elif x==3:
-#     print("x=3")
-# else:
-#     print("x<3")
+x=1
+if x>4:
+    print("x>4")
+elif x==3:
+    print("x=3")
+else:
+    print("x<3")
 
 #גירסא 2
 # num = int(input("enter a number : "))
@@ -23,17 +23,19 @@
 # else:
 #     print("number < 3")
 
-
-letter = input("ךמש לש הנושאר תואה תא סנכה : ")
-if letter=="ת":
-    print("רמות התא")
-elif letter=="א":
-    print("ירוא התא")
-elif letter=="ה":
-    print("הליה תא")
-elif letter=="מ":
-    print("יטומ התא")
-elif letter=="ד":
-    print("ידוד התא")
-else:
-    print("ךתוא ריכמ אל") 
+#המשתמש מכניס את האות הראשונה של שמו והפלט הוא שמו המלא
+# letter = input("ךמש לש הנושאר תואה תא סנכה : ")
+# if letter=="ת":
+#     print("רמות התא")
+# elif letter=="א":
+#     print("ירוא התא")
+# elif letter=="ה":
+#     print("הליה תא")
+# elif letter=="מ":
+#     print("יטומ התא")
+# elif letter=="ד":
+#     print("ידוד התא")
+# else:
+#     print("ךתוא ריכמ אל") 
+#פעילויות המשך - להוסיף שמות נוספים
+#שהזיהוי יהיה עפ"י 2 אותיות ראשונות-כדי שהזיהוי יהיה מדוייק יותר
