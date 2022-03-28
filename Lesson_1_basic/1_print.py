1844049783802bfd774d49d6a88aaf61410fc0b5
MotiMatika <motimatika@gmail.com>
2022-03-28 19:41:51 +0300

    5

--- Lesson_1_basic/1_print.py
+++ Lesson_1_basic/1_print.py
@@ -11,7 +11,24 @@
 # print("moti\n")
 # print("yair")
 
+# print("*"        *9)
+# print("","*"     *7)
+# print(" ","*"    *5)
+# print(" "*2,"*"  *3)
+# print(" "*3,"*"  *1)
 
+
+
+# print("*"        *9)
+# print("","*"     *7)
+# print(" ","*"    *5)
+# print(" "*2,"*"  *3)
+# print(" "*3,"*"  *1)
+# print(" "*3,"*"  *1)
+# print(" "*2,"*"  *3)
+# print(" ","*"    *5)
+# print("","*"     *7)
+# print("*"        *9)
 
 #H.W
 #  ? מהו הקוד שנותן את הפלט הבא 
@@ -24,15 +41,15 @@
 # print(2+3)
 # print("2+3")
 
-# ?מה ההבדל בין 2 הפקודות 
-print("moti\n")
-print("yair")
+# ?מה ההבדל בין הפקודות 
+# print("moti\n")
+# print("yair")
 
-print("\nmoti\n")
-print("yair")
+# print("\nmoti\n")
+# print("yair")
 
-print("\nmoti\nyair")
+# print("\nmoti\nyair")
 
-print("\nmoti yair")
+# print("\nmoti yair")
 
 
