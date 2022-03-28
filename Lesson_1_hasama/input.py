2f74344d7e436c2f0e0ae04c03d6d1879c959683
MotiMatika <motimatika@gmail.com>
2022-03-28 19:00:01 +0300

    5

---
+++ Lesson_1_hasama/input.py
@@ -0,0 +1,11 @@
+#המשתמש מקליד את גילו בשנים ומקבל את גילו בחודשים
+# years = int(input("\ntype your age in years: "))
+# months = years*36
+# print("your age in months : ",months)
+#פעילויות נוספות: להמיר לשבועות,ימים,דקות,שניות
+
+
+#משתמש מכניס מספר ופלט : שמו נכתב מספר הפעמים שהכניס
+# times = int(input("\ntype a number you want your name to be printed: "))
+# name = input("\ntype your name : ")
+# print(times*name)
---
+++ Lesson_1_hasama/print.py
@@ -0,0 +1,12 @@
+# # print(1+2)
+# # print(100/10)
+# # print("tomer")
+# # print("*     "*5)
+
+
+# print("2+3")
+# print(2+3)
+# print("2+3 = ",5)
+
+
+
