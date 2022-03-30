9d532634e2e5f73dc1c3fbec552f71e19bcc424a
MotiMatika <motimatika@gmail.com>
2022-03-30 15:55:44 +0300

    Update out_of_3.py

--- Lesson_2_If/The_Biggest/out_of_3.py
+++ Lesson_2_If/The_Biggest/out_of_3.py
@@ -38,3 +38,24 @@
 # b = input ("b = ")
 # c = input ("c = ")
 # print("\nThe Biggest Number is : ",max(a,b,c)) 
+
+#התכנית תמיין שלשה מספרים רק אם הם שונים
+# a = input ("a = ")
+# b = input ("b = ")
+# c = input ("c = ")
+# if a>b:
+#     if a>c :
+#         if b>c:
+#             print(" "*5,"a > b > c\n"," "*4,a,">",b,">",c)
+#         else:
+#             print(" "*5,"a > c > b\n"," "*4,a,">",c,">",b)
+#     else:
+#         print(" "*5,"c > a > b\n"," "*4,c,">",a,">",b)
+# else:
+#     if b>c:
+#         if a>c:
+#            print(" "*5,"b > a > c\n"," "*4,b,">",a,">",c) 
+#         else:
+#             print(" "*5,"b > c > a\n"," "*4,b,">",c,">",a)
+#     else:
+#         print(" "*5,"c > b > a\n"," "*4,c,">",b,">",a) 
