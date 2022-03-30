12a34a699d3714a4f349def75ddd3e077047606f
MotiMatika <motimatika@gmail.com>
2022-03-30 17:54:42 +0300

    5

---
+++ Lesson_2_If/The_Biggest/nisyonot/1.py
@@ -0,0 +1,31 @@
+# A = input("\nenter first number   ")
+# B = input("enter second number  ")
+# C = input("enter third number   ")
+# D = input("enter fourth number  ")
+# if A>B:
+#     if A>C:
+#         if A>D:
+#             print ("\nthis is the biggest number ",A)
+#         else:
+#             print ("\nthis is the biggest number ",D)
+#     else:
+#         if C>D:
+#             print("\nthis is the biggest number ",C)
+#         else:
+#             print("\nthis is the biggest number ",D)
+# else:
+#     if B>C:
+#         if B>D:
+#             print("\nthis is the biggest number ",B)
+#         else:
+#             print("\nthis is the biggest number ",D)
+#     else:
+#         if C>D:
+#             print("\nthis is the biggest number ",C)
+#         else:
+#             print("\nthis is the biggest number ",D)                            
+# print("\n",max(A,B,C,D))
+
+import random
+x=random.randint()
+print(x)
