f1fdf8ed6098490fd9b30708b73103cceb00409b
MotiMatika <98161728+MotiMatika@users.noreply.github.com>
2022-04-02 00:10:59 +0300

    Update string_3.py

--- Lesson_5_String/string_3.py
+++ Lesson_5_String/string_3.py
@@ -58,8 +58,20 @@
 
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-x=input("please enter a word ")
-print(x)
+# x=input("please enter a word ")
+# print(x)
+
+# new_str = ("today iffffffs friday")
+# num_of_words = new_str.split()
+# print(num_of_words)
+# last_length = 1
+# for i in num_of_words:
+#     new_length = len (i)
+#     if new_length>last_length:
+#         last_length = new_length
+#         longest_word = i
+
+# print(longest_word)
 
 
 
