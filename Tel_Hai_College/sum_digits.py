16bc45d590f6b25666a81359ca13df9b5a759987
MotiMatika <98161728+MotiMatika@users.noreply.github.com>
2022-04-02 23:13:08 +0300

    5

---
+++ Tel_Hai_College/sum_digits.py
@@ -0,0 +1,18 @@
+def sum_digits(num):
+    c=str(num)
+    x=len(c)
+    if x==1:
+        sum = num
+    elif x==2:
+        asarot = int(num/10)
+        ahadot = num-10*asarot
+        sum    = asarot+ahadot
+        #print(sum)
+    else:
+        meot   = int(num/100)
+        asarot = int((num-100*meot)/10)
+        ahadot = num-100*meot-10*asarot
+        sum = meot+asarot+ahadot
+        #print(sum)
+    return sum
+print(sum_digits(504))
