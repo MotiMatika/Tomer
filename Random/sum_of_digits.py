e979c29cbfd63eac4aa0aa3136e395731c7eab54
MotiMatika <98161728+MotiMatika@users.noreply.github.com>
2022-04-03 12:01:18 +0300

    5

---
+++ Random/sum_of_digits.py
@@ -0,0 +1,22 @@
+#הפונקציה מקבלת מספר אקראי ומסכמת את סכום ספרותיו
+#אפשר לקבוע אם רוצים את טווח המספרים האקראיים
+
+import random
+num = random.randint(1,999)
+print("\nThe random number is :",num)
+def sum_digits(num):
+    c=str(num)
+    length=len(c)
+    if length==1:
+        sum = num
+    elif length==2:
+        asarot = int(num/10)
+        ahadot = num-10*asarot
+        sum    = asarot+ahadot
+    else:
+        meot   = int(num/100)
+        asarot = int((num-100*meot)/10)
+        ahadot = num-100*meot-10*asarot
+        sum = meot+asarot+ahadot
+    return sum
+print("The sum of digits is :",sum_digits(num))
