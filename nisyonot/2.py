5caaef7a647c90aeb906e48e394351e063a9985f
MotiMatika <motimatika@gmail.com>
2022-04-03 20:26:46 +0300

    Update 2.py

--- nisyonot/2.py
+++ nisyonot/2.py
@@ -1,6 +1,24 @@
+import math
+import random
+from random import randint
 
-
-
-
+num=random.randint(1,99)
+print(num)
+st_num=str(num)
+length=len(st_num)
+print(length)
+sum=0
+while b!=0:
+    a=(num%10)
+    b=math.floor(num/10)
+    sum=sum+a
+    print(sum)
+    
+    # c=b%10
+    # d=math.floor(b/10)
+    # e=d%10
+    # f=math.floor(e/10)
+    # if f==0:
+print(sum)
 
 
