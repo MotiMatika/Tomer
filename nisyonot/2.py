c082428afec9d850fc66c08abd86aaa025f917d5
MotiMatika <motimatika@gmail.com>
2022-04-03 22:53:20 +0300

    Update 2.py

--- nisyonot/2.py
+++ nisyonot/2.py
@@ -1,24 +1,35 @@
-import math
-import random
-from random import randint
+# import math
+# import random
+# from random import randint
 
-num=random.randint(1,99)
-print(num)
-st_num=str(num)
-length=len(st_num)
-print(length)
-sum=0
-while b!=0:
-    a=(num%10)
-    b=math.floor(num/10)
-    sum=sum+a
-    print(sum)
+# num=random.randint(1,999)
+# print(num)
+# st_num=str(num)
+# length=len(st_num)
+# print(length)
+# sum=0
+# i=0
+# a=(num%10)
+# b=math.floor(num/10)
+# while i<length:
+#     sum=sum+a
     
-    # c=b%10
-    # d=math.floor(b/10)
-    # e=d%10
-    # f=math.floor(e/10)
-    # if f==0:
-print(sum)
+#     a=b%10
+#     b=math.floor(b/10)
+#     i=i+1
+    
+# print(sum)
 
+# print("*"*8)
+# print("*"," "*4,"*")
+# print("*","moti","*")
+# print("*"," "*4,"*")
+# print("*"*8)
+
+
+print("*"*15)
+print("*"," "*11,"*")    
+print("*"," "*4,"*"," "*4,"*")
+print("*"," "*2,"*"*9,"*"," "*2)
+print
 
