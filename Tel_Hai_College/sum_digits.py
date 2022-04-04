89fae22b79522dab47825c93d58c08dd63a85643
MotiMatika <motimatika@gmail.com>
2022-04-04 20:14:19 +0300

    Update sum_digits.py

--- Tel_Hai_College/sum_digits.py
+++ Tel_Hai_College/sum_digits.py
@@ -1,20 +1,20 @@
-# def sum_digits(num):
-#     c=str(num)
-#     x=len(c)
-#     if x==1:
-#         sum = num
-#     elif x==2:
-#         asarot = int(num/10)
-#         ahadot = num-10*asarot
-#         sum    = asarot+ahadot
-#         #print(sum)
-#     else:
-#         meot   = int(num/100)
-#         asarot = int((num-100*meot)/10)
-#         ahadot = num-100*meot-10*asarot
-#         sum = meot+asarot+ahadot
-#         #print(sum)
-#     return sum
-# print(sum_digits(504))
+def sum_digits(num):
+    c=str(num)
+    x=len(c)
+    if x==1:
+        sum = num
+    elif x==2:
+        asarot = int(num/10)
+        ahadot = num-10*asarot
+        sum    = asarot+ahadot
+        #print(sum)
+    else:
+        meot   = int(num/100)
+        asarot = int((num-100*meot)/10)
+        ahadot = num-100*meot-10*asarot
+        sum = meot+asarot+ahadot
+        #print(sum)
+    return sum
+print("The number is :",num,"\nSum of its digits is :",sum_digits(504))
 
 
