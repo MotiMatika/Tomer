1124e2db665076b58abfc94ffd676fd3eada9bda
MotiMatika <motimatika@gmail.com>
2022-04-09 17:50:05 +0300

    Update sum_of_digits.py

--- Random/sum_of_digits.py
+++ Random/sum_of_digits.py
@@ -1,22 +1,48 @@
 #הפונקציה מקבלת מספר אקראי ומסכמת את סכום ספרותיו
 #אפשר לקבוע אם רוצים את טווח המספרים האקראיים
+# רק למספר תלת ספרתי
+# import random
+# num = random.randint(1,999)
+# print("\nThe random number is :",num)
+# def sum_digits(num):
+#     convert_num=str(num)
+#     length=len(convert_num)
+#     if length==1:
+#         sum = num
+#     elif length==2:
+#         asarot = int(num/10)
+#         ahadot = num-10*asarot
+#         sum    = asarot+ahadot
+#     else:
+#         meot   = int(num/100)
+#         asarot = int((num-100*meot)/10)
+#         ahadot = num-100*meot-10*asarot
+#         sum = meot+asarot+ahadot
+#     return sum
+# print("The sum of digits is :",sum_digits(num))
 
+
+
+# הקוד מסכם ספרותיו של מספר אקראי                                
+# דרור   9.4.22                                 
 import random
-num = random.randint(1,999)
-print("\nThe random number is :",num)
-def sum_digits(num):
-    convert_num=str(num)
-    length=len(convert_num)
-    if length==1:
-        sum = num
-    elif length==2:
-        asarot = int(num/10)
-        ahadot = num-10*asarot
-        sum    = asarot+ahadot
-    else:
-        meot   = int(num/100)
-        asarot = int((num-100*meot)/10)
-        ahadot = num-100*meot-10*asarot
-        sum = meot+asarot+ahadot
+number = random.randint(1,10000)
+def sum_int(number):
+    print(number)
+    sum = 0
+    while number != 0:
+        sum = sum + number % 10   #מודולו משאיר את השארית לאחר החילוק ב 10
+        number = number // 10# הפעולה // לוקחת את התוצאה השלמה לאחר חילוק ב 10     
     return sum
-print("The sum of digits is :",sum_digits(num))
+
+print(sum_int(number))
+
+
+
+
+
+
+
+
+
+
