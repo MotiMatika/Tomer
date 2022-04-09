95b96b44bba58ca162fe32158bf1a0a1bb7ec7d3
MotiMatika <motimatika@gmail.com>
2022-04-09 21:42:26 +0300

    Update for_1.py

--- Lesson_8_Loops/For/for_1.py
+++ Lesson_8_Loops/For/for_1.py
@@ -1,3 +1,12 @@
 #קוד שמפרק את המילה לאותיות ורושם אותן כל אות בשורה חדשה
 #for x in "banana":
 #  print(x) 
+
+
+
+#קוד שמסכם כמות איברים בטווח שניתן לו
+# numbers = range(1,101)
+# sum = 0
+# for i in numbers:
+#     sum +=i
+# print("\nSum = ",sum)
