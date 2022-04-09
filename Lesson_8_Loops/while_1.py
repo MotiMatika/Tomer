e8e9c6cfa96fa14cfd1cfe73ecf028063058f5bd
MotiMatika <motimatika@gmail.com>
2022-04-09 21:29:58 +0300

    5

--- Lesson_8_Loops/while_1.py
+++ Lesson_8_Loops/while_1.py
@@ -1,3 +1,20 @@
+#הסבר על מבנה לולאת while
+# counter = 0
+# while counter < 3:
+#     print("\ninside loop")
+#     counter = counter+1
+# else:
+#     print("inside else")
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
                               #  while לולאת   									 
 #age = 16                    #השמת ערך
 #while age < 18:             #כל עןד הערך קטן מ 18 בצע
@@ -11,3 +28,30 @@
 #	age = age+1             #הוסף 1
 #print("not yet 18")      	#לא תודפס הודעה אלא רק לאחר שהתכנית תצא מהלולאה
                             #ואז תודפס פעם אחת	
+
+
+#   #הקוד מחבר כמות של איברים החל מ1
+# sum = 0
+# n   = 2#כמות האיברים בסידרה 
+# i   = 1
+# while i<=n:
+#     sum = sum+i
+#     i = i+1   #מונה את המחוברים
+# print("\nThe Sum of ",n,"numbers is", sum)    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
