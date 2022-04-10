92702917cc29c20001bdd88806bc1c0e1be48040
MotiMatika <motimatika@gmail.com>
2022-04-10 13:30:19 +0300

    Update basic.py

--- Random/basic.py
+++ Random/basic.py
@@ -9,20 +9,30 @@
 #2 הקודים הבאים מבצעים את אותה המשימה:
 #סיכום מספרים עוקבים לפי טווח שהוגדר
 #סיכום יחיד - מיקום פעולת ההדפסה
-import random
-num = random.randint(1,10)
-print("\n the random number is : ",num)
-sum = 0
-for i in range(0,num):
-    sum = sum+i
-print(sum)
+# import random
+# num = random.randint(1,10)
+# print("\n the random number is : ",num)
+# sum = 0
+# for i in range(0,num):
+#     sum = sum+i
+# print(sum)
 
 
 #סיכומי ביניים - מיקום פעולת ההדפסה
-import random
-num = random.randint(1,10)
-print("\n the random number is : ",num)
-sum = 0
-for i in range(0,num):
-    sum = sum+i
-    print(sum)
+# import random
+# num = random.randint(1,10)
+# print("\n the random number is : ",num)
+# sum = 0
+# for i in range(0,num):
+#     sum = sum+i
+#     print(sum)
+
+#הקוש מריץ מספרים אקראיים וקובע אם המספר זוגי או אי-זוגי
+# import random
+# num = random.randint(1,100)
+# print("\n"*5,"The Random number is :",num)
+# if num%2 == 0:
+#     print("\n"*2,num,"is pair")
+# else:
+#     print("\n"*2,num,"is not a pair")
+
