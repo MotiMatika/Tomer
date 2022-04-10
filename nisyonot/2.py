c089b39ff6bbd81633ccfd4cac7cfab5a93f49cb
MotiMatika <motimatika@gmail.com>
2022-04-10 07:35:40 +0300

    Update 2.py

--- nisyonot/2.py
+++ nisyonot/2.py
@@ -1,25 +1,10 @@
-# import math
-# import random
-# from random import randint
+# for i in range(1,30):
+#     if i%2 !=0  and i%3!=0 and i%5!=0 and i%7!=0: 
+#         print (i)
 
-# num=random.randint(1,999)
-# print(num)
-# st_num=str(num)
-# length=len(st_num)
-# print(length)
-# sum=0
-# i=0
-# a=(num%10)
-# b=math.floor(num/10)
-# while i<length:
-#     sum=sum+a
-    
-#     a=b%10
-#     b=math.floor(b/10)
-#     i=i+1
-    
-# print(sum)
-
-
-
-
+print("\n"*40)
+i = int(input("enter :"))
+if i%2 !=0  and i%3!=0 and i%5!=0 and i%7!=0: 
+    print(i," is a primery number")
+else:
+    print("not a primery")
