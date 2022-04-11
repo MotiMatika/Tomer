b3ea678d970528439f33d228da2af5648c6fd434
MotiMatika <98161728+MotiMatika@users.noreply.github.com>
2022-04-11 14:35:39 +0300

    Update 1.py

--- Lesson_10_Lambda/1.py
+++ Lesson_10_Lambda/1.py
@@ -1,7 +1,16 @@
+#   LAMBDA   הגדרת פונקציה כרצוני,ללא שם מוגדר
+
+#חישוב ריבועו של מספר
 # square = lambda x:x*x
 # result = square(5)
 # print(result) 
 
-multiply = lambda x,y:x*y
-result = multiply(5,6)
-print(result) 
+#חישוב מכפלה של 2 מספרים
+# multiply = lambda x,y:x*y
+# result = multiply(5,6)
+# print(result) 
+
+#חישוב סכום של 3 מספרים
+# sum = lambda a,b,c:a+b+c
+# result = sum(1,2,3)
+# print("\n",result)
