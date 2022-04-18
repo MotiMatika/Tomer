908af7e9df789bad60b2b37e0e2edd882a23c9f7
MotiMatika <motimatika@gmail.com>
2022-04-18 16:36:46 +0300

    Update call_func_from_file.py

--- nisyonot/call_func_from_file.py
+++ nisyonot/call_func_from_file.py
@@ -1,3 +1,8 @@
+#יצרתי 2 פונקציות  call_def_to_main בקובץ  
+#  ייבאתי את 2 הפונקציות  call_func_from_file בקובץ
+#נתתי ערכים לפונקציות ויישמתי אותן
 import call_def_to_main as t
-print(t.sum_num(10,2))
-print(t.div_num(10,2))
+SUM = t.sum_num(10,2)
+DIVIDED = t.div_num(10,2)
+print(3*SUM)
+print(DIVIDED-5)
