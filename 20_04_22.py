782f81ee2c0b5a388ca6c3cf28f0c30e7e7b1414
MotiMatika <motimatika@gmail.com>
2022-04-19 21:45:09 +0300

    Create 20_04_22.py

---
+++ 20_04_22.py
@@ -0,0 +1,30 @@
+#מה התכנית מבצעת 
+#1
+# a = 4*256
+# b = 8*128
+# c = 16*64
+# d = 32*32
+# e = 2*512
+# if a==b and a==c and a==d and a==e:
+#     print("\n      equal")
+# else:
+#     print("\n      not equal")
+
+
+#2
+# name = "moti"
+# if "i" in name:
+#     print("\ngood")
+# else:
+#     print("\nnot good")
+
+
+#3
+# two_digits = int(input("\nenter a number of 2 digits "))
+# asarot = int(two_digits/10)
+# ahadot = two_digits-asarot*10
+# print("Asarot digit = ",asarot,"\nAhadot digit = ",ahadot)
+
+
+
+
