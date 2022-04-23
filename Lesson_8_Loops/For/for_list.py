2612d982f624ae8f8aee3665d797716e04a1ac25
MotiMatika <motimatika@gmail.com>
2022-04-23 08:45:38 +0300

    Update for_list.py

--- Lesson_8_Loops/For/for_list.py
+++ Lesson_8_Loops/For/for_list.py
@@ -36,6 +36,37 @@
 # print(i)
 
 
+#פונקציה שמחפשת באופן סידרתי איבר ברשימה
+# list = [1,2,3,4,5,6]
+# num = 13
+# def search(list,num):
+#     for i in list:
+#         if i == num:
+#             return True
+        
+#     return False 
+
+# print(search(list,num))
+
+
+
+
+list = [1,2,3,4,5,6]
+num = 3
+def binary_search(list,nun):
+    left = 0
+    right = len(list)-1
+    while left<=right:
+        mid = (left+right)//2
+        if num == list[mid]:
+            return True
+        else:
+            if num<list[mid]:
+                right=mid-1
+            else:
+                left = mid+1
+    return False
+print(binary_search(list,num))
 
 
 
