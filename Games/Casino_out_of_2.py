b0e312887ae107f42eb943f6b89201ee7b0b3d02
MotiMatika <motimatika@gmail.com>
2022-04-23 21:42:26 +0300

    Create Casino_out_of_2.py

---
+++ Games/Casino_out_of_2.py
@@ -0,0 +1,13 @@
+import random
+#אקראיות בבחירת מספר.לספור מי ניצח אקראית את השני
+# moti = random.randint(1,100)
+# print("moti ,your number is :",moti)
+# pupil = random.randint(1,100)
+# print("pupil,your number is :",pupil)
+
+
+#אפשר לרמות - ותמיד לנצח
+moti = random.randint(50,100)
+print("moti ,your number is :",moti)
+pupil = random.randint(10,49)
+print("pupil,your number is :",pupil)
