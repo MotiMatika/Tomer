cf5e01ae38be95f5b73fe5fc500d200327c36455
MotiMatika <motimatika@gmail.com>
2022-05-06 10:59:12 +0300

    5

---
+++ Lesson_8_Loops/While/while_2.py
@@ -0,0 +1,25 @@
+#קוד שמחשב עצרת של מספר
+#גירסה 1
+
+# target = int(input("\nwhich number do you want to calculate ? "))
+# num=1
+# counter = 1
+
+# while counter <=target:
+#     num = counter*num
+#     counter = counter+1
+# print("\n",target,"! = ", num)        #הדפסה מחוץ ללולאה
+
+
+
+
+#קוד שמחשב עצרת של מספר
+#גירסה 2
+# target = int(input("\nwhich number do you want to calculate ? "))
+# num=1
+# counter = 1
+
+# while counter <=target:
+#     num = counter*num
+#     print(counter,"!  =",num)    #הדפסה בתוך הלולאה
+#     counter = counter+1
