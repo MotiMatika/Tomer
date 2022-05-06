d2e201f8ea0a4cd41ee59a06275990c6847ca439
MotiMatika <motimatika@gmail.com>
2022-05-06 10:55:37 +0300

    5

---
+++ Hw/HW_while.py
@@ -0,0 +1,66 @@
+#מה יקרה אם נריץ את הלולאה
+# while 1<3:
+#     print("Hellow")
+
+#מה יקרה אם נריץ את הלולאה
+# while 1>3:
+#     print("Hellow")
+
+
+#מה יקרה אם נריץ את הלולאה
+# while 1>3:
+#     print("Hellow")
+# print("\nend of loop")
+
+
+#רשום קוד בעזרת הלולאה ווייל שירשום את כל המספרים מ 1 עד 10
+
+
+# counter = 1
+# while counter <=10:
+#     print(counter)
+#     counter = counter+1
+
+#מה יגדיל את הטווח
+#מה יקרה אם פרינט יהיה מחוץ ללולאה 
+
+# counter = 1
+
+# while counter <=10:
+#     counter = counter+1
+
+# print("\n",counter)
+
+#רשום קוד שידפיס את כפולות ה-2 עד 20
+
+
+ #כפולות 2
+# n=2
+# while n<=10:
+#     print(n)
+#     n=n+2
+#מה יהפוך לכפולות 3
+#מה יגדיל את הטווח
+#מה יקרה אם פרינט יהיה מחוץ ללולאה   
+
+
+#קוד שמחשב עצרת של מספר
+
+# target = int(input("\nwhich number do you want to calculate ? "))
+# num=1
+# counter = 1
+
+# while counter <=target:
+#     num = counter*num
+#     counter = counter+1
+# print("\n",target,"! = ", num)
+
+
+target = int(input("\nwhich number do you want to calculate ? "))
+num=1
+counter = 1
+
+while counter <=target:
+    num = counter*num
+    print(counter,"!  =",num)
+    counter = counter+1
